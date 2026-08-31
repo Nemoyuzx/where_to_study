0d6f87152dbc2c694e2c8535481fdd80d7c47f5f
YZX_Studio (Nemoyuzx) <2099905168@qq.com>
2026-08-31 14:51:37 +0800

    release: bump Apple build 75

--- test/release-label.test.js
+++ test/release-label.test.js
@@ -90,7 +90,7 @@ test("all tracked client projects use the 0.2.8 pre-release version", () => {
   assert.match(nativeAndroid, /versionName = "0\.2\.8"/);
   assert.match(nativeAndroid, /versionCode = 44/);
   assert.match(nativeApple, /MARKETING_VERSION: "0\.2\.8"/);
-  assert.match(nativeApple, /CURRENT_PROJECT_VERSION: "74"/);
+  assert.match(nativeApple, /CURRENT_PROJECT_VERSION: "75"/);
   assert.match(nativeHarmony, /"versionName": "0\.2\.8"/);
   assert.match(nativeHarmony, /"versionCode": 1002013/);
   assert.match(tauriApple, /CFBundleShortVersionString: 0\.2\.8/);
