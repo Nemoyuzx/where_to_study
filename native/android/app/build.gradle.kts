1833f57e10b08b92273001f9042550bf72cea137
YZX_Studio (Nemoyuzx) <2099905168@qq.com>
2026-09-19 21:13:18 +0800

    chore(release): bump native builds for desktop calendar headers

--- native/android/app/build.gradle.kts
+++ native/android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.nemoyu.wheretostudy.nativeapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 51
+        versionCode = 52
         versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
